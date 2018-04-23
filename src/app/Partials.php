f690cf0ccfdc0a7cac04ac5171350829122c3e52
Joseph Martinsen <joseph@martinsen.com>
2018-04-23 01:06:51 -0500

    index.php UX in place

--- src/app/Partials.php
+++ src/app/Partials.php
@@ -1,12 +1,5 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: joseph
- * Date: 4/21/18
- * Time: 2:40 PM
- */
-
-/**
  * HtmlHeader
  * @param $title string Title of the page
  * @return string default HTML <head>
@@ -14,15 +7,47 @@
 function HtmlHeader($title)
 {
     return <<< HTML
-    <head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>P2|{$title}</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" media="screen" href="/assets/css/main.css" />
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-        <script src="/assets/js/main.js"></script>
-    </head>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>P2|{$title}</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!--Bootstrap CSS -->
+    <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">-->
+
+    <!-- Local CSS-->
+    <link rel="stylesheet" type="text/css" media="screen" href="/assets/css/main.css" />
+    
+    <!-- Bootstrap JSS-->
+    <!--<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>-->
+    <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>-->
+    <!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>-->
+
+    <!-- Google Charts -->
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    
+    <!-- Local JS-->
+    <script src="/assets/js/main.js"></script>
+</head>
+HTML;
+}
+
+/**
+ * Sidebar
+ * @return string
+ */
+function Sidebar() {
+    return <<< HTML
+    <div id="mySidenav" class="hidden">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <a href="/">Home</a>
+        <a href="#">Admin</a>
+    </div>
+    <nav class="nav">
+        <ul>
+             <li><span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</span></li>
+             <li><h1>Vehicle Garage Counter</h1></li>   
+        </ul>
+    </nav>
 HTML;
 }
--- src/app/index.php
+++ src/app/index.php
@@ -2,7 +2,115 @@
 <!DOCTYPE html>
 <html>
 <?= HtmlHeader("Home"); ?>
+
 <body>
-<?php phpinfo() ?>
+<?= Sidebar(); ?>
+<div class="container">
+    <div class="section">
+        <h1 class="title">Overall Data</h1>
+        <div class="grid-container">
+            <div>
+                <h1 class="grid-title">Total Count</h1>
+                <p class="grid-count">12</p>
+            </div>
+            <div>
+                <h1 class="grid-title">Mean</h1>
+                <p class="grid-count">55</p>
+            </div>
+            <div>
+                <h1 class="grid-title">Mean</h1>
+                <p class="grid-count">55</p>
+            </div>
+            <div>
+                <h1 class="grid-title">Mean</h1>
+                <p class="grid-count">55</p>
+            </div>
+            <div>
+                <h1 class="grid-title">Max Parking Lot</h1>
+                <p class="grid-count">55</p>
+            </div>
+            <div>
+                <h1 class="grid-title">Mean</h1>
+                <p class="grid-count">55</p>
+            </div>
+            <div>
+                <h1 class="grid-title">Mean</h1>
+                <p class="grid-count">55</p>
+            </div>
+            <div>
+                <h1 class="grid-title">Mean</h1>
+                <p class="grid-count">55</p>
+            </div>
+            <div>
+                <h1 class="grid-title">Mean</h1>
+                <p class="grid-count">55</p>
+            </div>
+        </div>
+    </div>
+
+    <hr/>
+
+    <div class="section">
+        <div id="chart_div"></div>
+    </div>
+
+    <hr/>
+
+    <div class="section">
+        <h1 class="title">Select Lot</h1>
+        <div class="lot-container">
+            <div>Lot 45</div>
+            <div>Lot 45</div>
+            <div>Lot 45</div>
+            <div>Lot 45</div>
+        </div>
+    </div>
+</div>
+<script>
+    google.charts.load('current', {packages: ['corechart', 'bar']});
+    google.charts.setOnLoadCallback(drawTrendlines);
+
+    function drawTrendlines() {
+        var data = new google.visualization.DataTable();
+        data.addColumn('timeofday', 'Time of Day');
+        data.addColumn('number', 'Motivation Level');
+        data.addColumn('number', 'Energy Level');
+
+        data.addRows([
+            [{v: [8, 0, 0], f: '8 am'}, 1, .25],
+            [{v: [9, 0, 0], f: '9 am'}, 2, .5],
+            [{v: [10, 0, 0], f:'10 am'}, 3, 1],
+            [{v: [11, 0, 0], f: '11 am'}, 4, 2.25],
+            [{v: [12, 0, 0], f: '12 pm'}, 5, 2.25],
+            [{v: [13, 0, 0], f: '1 pm'}, 6, 3],
+            [{v: [14, 0, 0], f: '2 pm'}, 7, 4],
+            [{v: [15, 0, 0], f: '3 pm'}, 8, 5.25],
+            [{v: [16, 0, 0], f: '4 pm'}, 9, 7.5],
+            [{v: [17, 0, 0], f: '5 pm'}, 10, 10],
+        ]);
+
+        var options = {
+            title: 'Motivation and Energy Level Throughout the Day',
+            trendlines: {
+                0: {type: 'linear', lineWidth: 5, opacity: .3},
+                1: {type: 'exponential', lineWidth: 10, opacity: .3}
+            },
+            hAxis: {
+                title: 'Time of Day',
+                format: 'h:mm a',
+                viewWindow: {
+                    min: [7, 30, 0],
+                    max: [17, 30, 0]
+                }
+            },
+            vAxis: {
+                title: 'Rating (scale of 1-10)'
+            }
+        };
+
+        var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+    }
+</script>
 </body>
 </html>
